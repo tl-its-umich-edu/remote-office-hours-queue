384ccea076930ded79b706fe8a36df31c3e8a550
James Ostrander <jlost@umich.edu>
2020-03-12 18:13:20 -0400

    Use re_path for oidc route

--- src/officehours/urls.py
+++ src/officehours/urls.py
@@ -14,9 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import re_path, path, include
 
 urlpatterns = [
-    path('oidc/', include('mozilla_django_oidc.urls')),
+    re_path(r'^oidc/', include('mozilla_django_oidc.urls')),
     path('admin/', admin.site.urls),
 ]
