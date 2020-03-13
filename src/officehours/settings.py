6cc94e07de375ad5eb78cf5e2fe6379ddbb3a303
James Ostrander <jlost@umich.edu>
2020-03-13 09:17:56 -0400

    Add feedback

--- src/officehours/settings.py
+++ src/officehours/settings.py
@@ -124,6 +124,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'bluejeans_queue.context_processors.feedback',
             ],
         },
     },
@@ -178,3 +179,5 @@ USE_TZ = True
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+FEEDBACK_EMAIL = os.getenv('FEEDBACK_EMAIL')
