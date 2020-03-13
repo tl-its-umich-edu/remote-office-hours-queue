808dae66e8296b59dc3efc673e67fe301bb72aa1
Kris Steinhoff <steinhof@umich.edu>
2020-03-13 14:15:12 -0400

    Only use API if BlueJeans is configured

--- src/bluejeans_queue/models.py
+++ src/bluejeans_queue/models.py
@@ -4,11 +4,13 @@ from django.contrib.auth.models import User
 
 from .bluejeans import Bluejeans
 
-
-bluejeans = Bluejeans(
-    client_id=settings.BLUEJEANS_CLIENT_ID,
-    client_secret=settings.BLUEJEANS_CLIENT_SECRET,
-)
+if settings.BLUEJEANS_CLIENT_ID and settings.BLUEJEANS_CLIENT_SECRET:
+    bluejeans = Bluejeans(
+        client_id=settings.BLUEJEANS_CLIENT_ID,
+        client_secret=settings.BLUEJEANS_CLIENT_SECRET,
+    )
+else:
+    bluejeans = None
 
 
 class BluejeansMeeting(models.Model):
@@ -27,22 +29,24 @@ class BluejeansMeeting(models.Model):
         return bluejeans.get_user(self.owner.email)
 
     def save(self, *args, **kwargs):
-        if not self.bjn_meeting_id:
-            user = bluejeans.get_user(user_email=self.owner.email)
-            self.bjn_user_id = user['id']
+        if bluejeans:
+            if not self.bjn_meeting_id:
+                user = bluejeans.get_user(user_email=self.owner.email)
+                self.bjn_user_id = user['id']
 
-            meeting = bluejeans.create_meeting(self.bjn_user_id)
-            self.bjn_meeting_id = meeting['id']
-            self.bjn_meeting_url = \
-                f'https://bluejeans.com/{meeting["numericMeetingId"]}'
+                meeting = bluejeans.create_meeting(self.bjn_user_id)
+                self.bjn_meeting_id = meeting['id']
+                self.bjn_meeting_url = \
+                    f'https://bluejeans.com/{meeting["numericMeetingId"]}'
 
         super().save(*args, **kwargs)
 
     def delete(self, *args, **kwargs):
-        bluejeans.delete_meeting(self.bluejeans_user['id'],
-                                 self.bjn_meeting_id)
+        if bluejeans:
+            bluejeans.delete_meeting(self.bluejeans_user['id'],
+                                     self.bjn_meeting_id)
 
-        super().delete(*args, **kwargs)
+            super().delete(*args, **kwargs)
 
     def __str__(self):
         return f'id={self.bjn_meeting_id} user_email={self.owner.email}'
