aef76c57ec41ecbbd1312d108f426d3fe0696453
James Ostrander <jlost@umich.edu>
2020-03-12 21:36:45 -0400

    Stub out queue view

--- src/bluejeans_queue/views.py
+++ src/bluejeans_queue/views.py
@@ -3,3 +3,7 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = 'bluejeans_queue/index.html'
+
+
+class QueueView(TemplateView):
+    template_name = 'bluejeans_queue/queue.html'
