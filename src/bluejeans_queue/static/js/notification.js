478e53eaddfa75014214e99a560ca259e7c4323b
James Ostrander <jlost@umich.edu>
2020-03-14 18:54:40 -0400

    Add notification log

--- src/bluejeans_queue/static/js/notification.js
+++ src/bluejeans_queue/static/js/notification.js
@@ -1,8 +1,39 @@
+function clearLastNotification() {
+    window.localStorage.setItem(url, '');
+}
+
+function getLastNotification() {
+    const url = location.href;
+    const stored = window.localStorage.getItem(url);
+    if (!stored) return undefined;
+    parsed = JSON.parse(stored);
+    parsed.timestamp = new Date(parsed.timestamp);
+    return parsed;
+}
+
+function setLastNotification(type) {
+    const url = location.href;
+    const n = {
+        timestamp: new Date(),
+        type: type,
+    };
+    window.localStorage.setItem(url, JSON.stringify(n));
+}
+
+function isNotificationRepeat(type) {
+    const last = getLastNotification();
+    if (!last) return false;
+    const now = new Date();
+    return last.type === type
+        && new Date(last.timestamp.getTime() + 10 * 60 * 1000) > now;
+}
+
 function notifyWithElement(selector) {
     const elem = document.querySelector(selector);
-    if (!!elem) {
-        new Notification(elem.innerText);
-    }
+    if (!elem) return;
+    if (isNotificationRepeat(selector)) return;
+    new Notification(elem.innerText);
+    setLastNotification(selector);
 }
 
 function requestNotify() {
