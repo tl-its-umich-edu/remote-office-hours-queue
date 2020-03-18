48d81b5649edf0eea3543bbad0abc0401047ad27
Patrick Steffes <psteffes@umich.edu>
2020-03-18 16:09:10 -0400

    add queue details

--- src/bluejeans_queue/management/commands/queuelist.py
+++ src/bluejeans_queue/management/commands/queuelist.py
@@ -1,10 +1,15 @@
 from django.core.management.base import BaseCommand, CommandError
 from bluejeans_queue.models import BluejeansMeeting
 
+
 class Command(BaseCommand):
     help = 'List all active queues'
 
     def handle(self, *args, **options):
-        #self.stdout.write(str(BluejeansMeeting.objects.filter(is_active=True)))
-        for meeting in BluejeansMeeting.objects.filter(is_active=True):
-            self.stdout.write(str(meeting))
+        meetings = BluejeansMeeting.objects.filter(is_active=True)
+        for meeting in meetings.order_by('owner').distinct('owner'):
+            queue_details = {
+                'owner': meeting.owner.username,
+                'in_line': meeting.owner.owner.filter(is_active=True).count(),
+            }
+            self.stdout.write(str(queue_details))
