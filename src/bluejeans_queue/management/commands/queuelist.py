0426a4c33bc1105e8c6a0a00e1d85b966fe84678
Patrick Steffes <psteffes@umich.edu>
2020-03-18 17:31:07 -0400

    add --all flag and update filtering

--- src/bluejeans_queue/management/commands/queuelist.py
+++ src/bluejeans_queue/management/commands/queuelist.py
@@ -1,15 +1,25 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.contrib.auth.models import User
 from bluejeans_queue.models import BluejeansMeeting
+
+
+User.objects.filter(owner__is_active=True).distinct()
 
 
 class Command(BaseCommand):
     help = 'List all active queues'
 
+    def add_arguments(self, parser):
+        parser.add_argument('--all', action='store_true', help='get all active and inactive queues')
+
     def handle(self, *args, **options):
-        meetings = BluejeansMeeting.objects.filter(is_active=True)
-        for meeting in meetings.order_by('owner').distinct('owner'):
+        if options['all']:
+            owners = User.objects.all()
+        else:
+            owners = User.objects.filter(owner__is_active=True).distinct()
+        for owner in owners:
             queue_details = {
-                'owner': meeting.owner.username,
-                'in_line': meeting.owner.owner.filter(is_active=True).count(),
+                'owner': owner.email,
+                'in_line': owner.owner.filter(is_active=True).count(),
             }
             self.stdout.write(str(queue_details))
