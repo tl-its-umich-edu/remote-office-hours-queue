cf33fc55d06ad8ac7184d972da47b8d199cc4962
James Ostrander <jlost@umich.edu>
2020-03-19 10:25:28 -0400

    Tweak schema; auto add Host to user

--- src/officehours_api/models.py
+++ src/officehours_api/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.dispatch import receiver
+from django.db.models.signals import post_save
 
 
 class Host(models.Model):
@@ -22,15 +24,31 @@ class Queue(models.Model):
 
 
 class Meeting(models.Model):
-    queue = models.ForeignKey(Queue, on_delete=models.CASCADE, related_name='queue')
+    queue = models.ForeignKey(
+        Queue, on_delete=models.CASCADE,
+        related_name='queue',
+        null=True
+    )
 
 
 class Attendee(models.Model):
-    user = models.OneToOneField(
+    user = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
     )
-    meeting = models.ForeignKey(Meeting, on_delete=models.CASCADE, related_name='meeting')
+    meeting = models.ForeignKey(
+        Meeting,
+        on_delete=models.CASCADE,
+        related_name='meeting'
+    )
 
     def __str__(self):
         return f'user={self.user.username}'
+
+
+@receiver(post_save, sender=User)
+def post_save_user_signal_handler(sender, instance, created, **kwargs):
+    try:
+        instance.host
+    except User.host.RelatedObjectDoesNotExist:
+        instance.host = Host.objects.create(user=instance)
