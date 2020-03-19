011cf0dddcac9f355a755e45d05220b41d231a64
James Ostrander <jlost@umich.edu>
2020-03-19 11:15:35 -0400

    Remove unused import

--- src/assets/src/index.tsx
+++ src/assets/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, withRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { AppRouter as App } from './containers/app';
 
 export interface User {
