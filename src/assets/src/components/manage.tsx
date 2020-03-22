f071721c300845f3f61753032fc36bab09b3bffe
James Ostrander <jlost@umich.edu>
2020-03-21 22:47:37 -0400

    Remove unused imports

--- src/assets/src/components/manage.tsx
+++ src/assets/src/components/manage.tsx
@@ -1,8 +1,8 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import { getQueuesFake as apiGetQueues, addQueueFake as apiAddQueue, removeQueueFake as apiRemoveQueue } from "../services/api";
-import { User, ManageQueue, Meeting } from "../models";
-import { UserDisplay, RemoveButton, AddButton } from "./common";
+import { User, ManageQueue } from "../models";
+import { RemoveButton, AddButton } from "./common";
 import { Link } from "react-router-dom";
 
 interface QueueListProps {
