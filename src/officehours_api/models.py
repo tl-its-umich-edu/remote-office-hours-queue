fcf56c5fa5f295272f147113277bb5bc27c96000
James Ostrander <jlost@umich.edu>
2020-07-14 10:35:42 -0400

    Remove non-functional predelete hook

--- src/officehours_api/models.py
+++ src/officehours_api/models.py
@@ -120,18 +120,10 @@ class Attendee(SafeDeleteModel):
     def __str__(self):
         return f'user={self.user.username}'
 
+
 @receiver(post_save, sender=User)
 def post_save_user_signal_handler(sender, instance: User, created, **kwargs):
     try:
         instance.profile
     except User.profile.RelatedObjectDoesNotExist:
         instance.profile = Profile.objects.create(user=instance)
-
-
-@receiver(pre_softdelete, sender=Meeting)
-def pre_delete_meeting_signal_handler(sender, instance: Meeting, **kwargs):
-    if instance.backend_type:
-        backend = globals()[instance.backend_type]
-        if backend:
-            backend.remove_user_meeting(instance.backend_metadata)
-            instance.backend_metadata.clear()
