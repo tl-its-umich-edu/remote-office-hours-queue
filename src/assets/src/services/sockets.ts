16a48316e212a64208bb56619f5f1dd5786663ff
James Ostrander <jlost@umich.edu>
2020-07-23 16:16:43 -0400

    Use ws/wss depending on http/https

--- src/assets/src/services/sockets.ts
+++ src/assets/src/services/sockets.ts
@@ -1,16 +1,20 @@
 import { useWebSocket } from "../hooks/useWebSocket";
 import { QueueHost, QueueAttendee, User, MyUser } from "../models";
 
+const getProtocol = () => {
+    return location.protocol === "https:" ? "wss:" : "ws:";
+}
+
 export const useQueueWebSocket = (queue_id: number, update: (q: QueueHost | QueueAttendee | undefined) => void) => {
     return useWebSocket(
-        `ws://${location.host}/ws/queues/${queue_id}/`,
+        `${getProtocol()}//${location.host}/ws/queues/${queue_id}/`,
         update,
     );
 }
 
 export const useUsersWebSocket = (update: (users: User[]) => void) => {
     return useWebSocket(
-        `ws://${location.host}/ws/users/`,
+        `${getProtocol()}//${location.host}/ws/users/`,
         update,
         false,
     );
@@ -19,7 +23,7 @@ export const useUsersWebSocket = (update: (users: User[]) => void) => {
 export const useUserWebSocket = (user_id: number | undefined, update: (user: User | MyUser) => void) => {
     return typeof user_id === "number"
         ? useWebSocket(
-            `ws://${location.host}/ws/users/${user_id}/`,
+            `${getProtocol()}//${location.host}/ws/users/${user_id}/`,
             update,
         )
         : undefined;
