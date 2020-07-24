24326bbb6cd75df4d91122f94c043dbcac7f72b5
James Ostrander <jlost@umich.edu>
2020-07-24 15:52:45 -0400

    Display error message if queue is deleted

--- src/assets/src/services/sockets.ts
+++ src/assets/src/services/sockets.ts
@@ -5,26 +5,30 @@ const getProtocol = () => {
     return location.protocol === "https:" ? "wss:" : "ws:";
 }
 
-export const useQueueWebSocket = (queue_id: number, update: (q: QueueHost | QueueAttendee | undefined) => void) => {
+export const useQueueWebSocket = (queue_id: number, onUpdate: (q: QueueHost | QueueAttendee | undefined) => void) => {
     return useWebSocket(
         `${getProtocol()}//${location.host}/ws/queues/${queue_id}/`,
-        update,
+        onUpdate,
+        (setError) => {
+            onUpdate(undefined);
+            setError(new Error("The queue was deleted."));
+        },
     );
 }
 
-export const useUsersWebSocket = (update: (users: User[]) => void) => {
+export const useUsersWebSocket = (onUpdate: (users: User[]) => void) => {
     return useWebSocket(
         `${getProtocol()}//${location.host}/ws/users/`,
-        update,
-        false,
+        onUpdate,
     );
 }
 
-export const useUserWebSocket = (user_id: number | undefined, update: (user: User | MyUser) => void) => {
+export const useUserWebSocket = (user_id: number | undefined, onUpdate: (user: User | MyUser) => void) => {
     return typeof user_id === "number"
         ? useWebSocket(
             `${getProtocol()}//${location.host}/ws/users/${user_id}/`,
-            update,
+            onUpdate,
+            () => onUpdate(undefined!),
         )
         : undefined;
 }
