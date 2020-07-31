173d15c170fa31bef4aaf95f83a575b81f9790a7
James Ostrander <jlost@umich.edu>
2020-07-31 15:32:27 -0400

    WIP zoom backend

--- src/officehours_ui/urls.py
+++ src/officehours_ui/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf import settings
 
 from .views import SpaView
+from officehours_api import backends
 
 
 urlpatterns = [
@@ -13,4 +15,13 @@ urlpatterns = [
     path('search/<str:term>/', SpaView.as_view()),
     path('preferences/', SpaView.as_view(), name='preferences'),
     path('add_queue/', SpaView.as_view(), name='add_queue'),
+]
+backend_classes = {
+    backend_name: getattr(getattr(backends, backend_name), 'Backend')
+    for backend_name in settings.ENABLED_BACKENDS
+}
+urlpatterns += [
+    path(f'authorize/{backend_name}/', backend.auth_callback, name=backend_name)
+    for backend_name, backend in backend_classes.items()
+    if hasattr(backend, 'auth_callback')
 ]
