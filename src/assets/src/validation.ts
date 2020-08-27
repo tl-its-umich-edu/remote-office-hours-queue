b4c91973dc947481408280782766151a8c6723b9
Sam Sciolla <ssciolla@umich.edu>
2020-08-27 16:39:16 -0400

    Remove extra test schema methods; add semicolons

--- src/assets/src/validation.ts
+++ src/assets/src/validation.ts
@@ -13,38 +13,42 @@ function getMaxLimit (description: SchemaDescription): number | undefined {
     return matches.length === 1 ? matches[0].params.max : undefined;
 }
 
-function createRemainingCharsMessage (data: any) {
-    // Type of input could be StringLocale['max'], but wasn't sure how to handle undefineds
+function createRemainingCharsMessage (data: any): string | undefined {
+    // Type of input could be StringLocale, but wasn't sure how to handle undefineds
     // https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/yup/index.d.ts
-    const remaining = data.max - data.value.length;
-    const charsRemaining = (remaining > 0) ? remaining : 0;
-    const charsOver = (remaining < 0) ? ` (${remaining * -1} over limit)` : '';
-    return `Remaining characters: ${charsRemaining}/${data.max}${charsOver}`;
+    if (data && 'max' in data && 'value' in data) {
+        const remaining = data!.max - data!.value.length;
+        const charsRemaining = (remaining > 0) ? remaining : 0;
+        const charsOver = (remaining < 0) ? ` (${remaining * -1} over limit)` : '';
+        return `Remaining characters: ${charsRemaining}/${data.max}${charsOver}`;
+    } else {
+        return undefined;
+    }
 }
 
 // Schemas
 
-const queueTitleSchema = string().required('This field may not be left blank.').max(100, createRemainingCharsMessage).min(2);
-const queueDescriptSchema = string().max(1000, createRemainingCharsMessage).max(1000, '');
-const meetingAgendaSchema = string().max(100, createRemainingCharsMessage);
+const blankText = 'This field may not be left blank.'
 
+export const queueTitleSchema = string().required(blankText).max(100, createRemainingCharsMessage);
+export const queueDescriptSchema = string().max(1000, createRemainingCharsMessage);
+export const meetingAgendaSchema = string().max(100, createRemainingCharsMessage);
 
 // Type validators
 
-interface ValidationResult {
+export interface ValidationResult {
     isInvalid: boolean;
-    messages: ReadonlyArray<string>
+    messages: ReadonlyArray<string>;
 }
 
-function validateString (value: string, schema: StringSchema, showRemaining: boolean): ValidationResult {
+export function validateString (value: string, schema: StringSchema, showRemaining: boolean): ValidationResult {
     let messages = Array();
     let isInvalid = false;
     try {
         // We could also do this asynchronously with .validate, but wasn't sure if that was needed?
-        console.log(schema.validateSync(value))
-        const maxLimit = getMaxLimit(schema.describe())
+        const maxLimit = getMaxLimit(schema.describe());
         if (showRemaining && maxLimit) {
-            messages.push(createRemainingCharsMessage({'value': value, 'max': maxLimit}))
+            messages.push(createRemainingCharsMessage({'value': value, 'max': maxLimit}));
         }
     } catch (error) {
         console.log(error.name);
@@ -54,5 +58,3 @@ function validateString (value: string, schema: StringSchema, showRemaining: boo
     }
     return {'isInvalid': isInvalid, 'messages': messages};
 }
-
-export { validateString, queueDescriptSchema, meetingAgendaSchema, queueTitleSchema };
