795f13eb99d41f2ec9f0d067b25300dd7844c7fc
Sam Sciolla <ssciolla@umich.edu>
2020-09-11 09:53:30 -0400

    Refactor QueueList as QueueTable

--- src/assets/src/components/search.tsx
+++ src/assets/src/components/search.tsx
@@ -4,7 +4,7 @@ import { useState, useEffect } from "react";
 import { QueueBase } from "../models";
 import { usePromise } from "../hooks/usePromise";
 import { searchQueue as apiSearchQueue } from "../services/api";
-import { LoadingDisplay, ErrorDisplay, FormError, Breadcrumbs, QueueList } from "./common";
+import { LoadingDisplay, ErrorDisplay, FormError, Breadcrumbs, QueueTable } from "./common";
 import { redirectToLogin } from "../utils";
 import { PageProps } from "./page";
 
@@ -39,7 +39,7 @@ export function SearchPage(props: PageProps<SearchPageParams>) {
                     No results were found for "{term}".
                 </p>
             )
-            : <QueueList queues={searchResults} />
+            : <QueueTable queues={searchResults} />
     const redirectAlert = props.location.search.includes("redirected=true") && !/^\d+$/.exec(term)
         && (
             <p className="alert alert-warning">
