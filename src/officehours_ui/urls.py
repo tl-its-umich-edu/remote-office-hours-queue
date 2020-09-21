f5362f2a5e689320498217239d91a73316ab8ad0
James Ostrander <jlost@umich.edu>
2020-09-21 10:58:44 -0400

    Use 3 legged oauth views

--- src/officehours_ui/urls.py
+++ src/officehours_ui/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 from django.conf import settings
 
-from .views import SpaView
-from officehours_api import backends
+from .views import SpaView, AuthPromptView, auth_callback_view
 
 
 urlpatterns = [
@@ -15,13 +14,6 @@ urlpatterns = [
     path('search/<str:term>/', SpaView.as_view()),
     path('preferences/', SpaView.as_view(), name='preferences'),
     path('add_queue/', SpaView.as_view(), name='add_queue'),
-]
-backend_classes = {
-    backend_name: getattr(getattr(backends, backend_name), 'Backend')
-    for backend_name in settings.ENABLED_BACKENDS
-}
-urlpatterns += [
-    path(f'authorize/{backend_name}/', backend.auth_callback, name=backend_name)
-    for backend_name, backend in backend_classes.items()
-    if hasattr(backend, 'auth_callback')
+    path('auth/<backend_name>/', AuthPromptView.as_view(), name='auth_prompt'),
+    path('callback/<backend_name>/', auth_callback_view, name='auth_callback'),
 ]
