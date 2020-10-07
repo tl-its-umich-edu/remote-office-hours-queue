83be232a02aa50e172922ea4082fa7ffda3f13c4
James Ostrander <jlost@umich.edu>
2020-10-07 12:08:52 -0400

    Working basics of attendee notification

--- src/officehours_api/notifications.py
+++ src/officehours_api/notifications.py
@@ -1,20 +1,21 @@
 from django.conf import settings
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_delete
 
 from twilio.rest import Client as TwilioClient
 
 from officehours_api.models import Queue, Meeting
 
 
-if settings.TWILIO_ACCOUNT_SID and settings.TWILIO_AUTH_TOKEN:
+if settings.TWILIO_ACCOUNT_SID and settings.TWILIO_AUTH_TOKEN and settings.TWILIO_PHONE_FROM:
     # TODO: Generate URL properly
     def notify_next_in_line(next_in_line: Meeting):
-        print('notify_next_in_line')
-        for phone in (u.profile.phone_number for u in next_in_line.attendees if u.profile.phone_number):
+        for phone in (u.profile.phone_number for u in next_in_line.attendees.all() if u.profile.phone_number):
+            print(phone)
             client = TwilioClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
             message = client.messages.create(
                 to=phone,
+                from_=settings.TWILIO_PHONE_FROM,
                 body=(
                     f"You're next in line! Please visit "
                     f"https://localhost:8003/queue/{next_in_line.queue.id}/ "
@@ -23,23 +24,12 @@ if settings.TWILIO_ACCOUNT_SID and settings.TWILIO_AUTH_TOKEN:
             )
             print(message.sid)
 
-    @receiver(post_save, sender=Meeting)
-    def trigger_notification_create(sender, instance: Meeting, created, **kwargs):
-        print('trigger_notification_create')
-        if instance.deleted:
-            return
-        if not created:
-            return
-        print('place_in_line', instance.line_place)
-        if instance.line_place == 0:
-            notify_next_in_line(instance)
-
-    @receiver(post_delete, sender=Meeting)
+    @receiver(pre_delete, sender=Meeting)
     def trigger_notification_delete(sender, instance: Meeting, **kwargs):
-        print('trigger_notification_delete')
+        print('trigger_notification_delete', instance)
         print('place_in_line', instance.line_place)
         if instance.line_place == 0:
-            if instance.meeting_set.count() <= 1:
+            if instance.queue.meeting_set.count() <= 1:
                 return
             next_in_line = instance.queue.meeting_set.order_by('id')[1]
             notify_next_in_line(next_in_line)
