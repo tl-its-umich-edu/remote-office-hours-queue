cbf7007b396330fe405067a536d440faebfae9ee
James Ostrander <jlost@umich.edu>
2020-10-12 16:19:29 -0400

    Use override_settings

--- src/officehours_api/notifications.py
+++ src/officehours_api/notifications.py
@@ -12,59 +12,63 @@ from officehours_api.models import Queue, Meeting
 
 logger = logging.getLogger(__name__)
 
-if settings.TWILIO_ACCOUNT_SID and settings.TWILIO_AUTH_TOKEN and settings.TWILIO_PHONE_FROM:
+
+def notify_next_in_line(next_in_line: Meeting):
+    if not (settings.TWILIO_ACCOUNT_SID and settings.TWILIO_AUTH_TOKEN and settings.TWILIO_PHONE_FROM):
+        return
     twilio = TwilioClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
-
-    def notify_next_in_line(next_in_line: Meeting):
-        phone_numbers = list(
-            u.profile.phone_number for u in
-            next_in_line.attendees_with_phone_numbers
+    phone_numbers = list(
+        u.profile.phone_number for u in
+        next_in_line.attendees_with_phone_numbers
+    )
+    for p in phone_numbers:
+        logger.info('notify_next_in_line: %s', p)
+        domain = Site.objects.get_current().domain
+        queue_path = reverse('queue', kwargs={'queue_id': next_in_line.queue.id})
+        queue_url = f"https://{domain}{queue_path}"
+        twilio.messages.create(
+            to=p,
+            from_=settings.TWILIO_PHONE_FROM,
+            body=(
+                f"You're next in line! Please visit {queue_url} "
+                f"for instructions to join."
+            ),
         )
-        for p in phone_numbers:
-            logger.info('notify_next_in_line: %s', p)
-            domain = Site.objects.get_current().domain
-            queue_path = reverse('queue', kwargs={'queue_id': next_in_line.queue.id})
-            queue_url = f"https://{domain}{queue_path}"
-            twilio.messages.create(
-                to=p,
-                from_=settings.TWILIO_PHONE_FROM,
-                body=(
-                    f"You're next in line! Please visit {queue_url} "
-                    f"for instructions to join."
-                ),
-            )
 
-    def notify_queue_no_longer_empty(first: Meeting):
-        phone_numbers = list(
-            h.profile.phone_number for h in
-            first.queue.hosts_with_phone_numbers
+def notify_queue_no_longer_empty(first: Meeting):
+    if not (settings.TWILIO_ACCOUNT_SID and settings.TWILIO_AUTH_TOKEN and settings.TWILIO_PHONE_FROM):
+        return
+    twilio = TwilioClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
+    phone_numbers = list(
+        h.profile.phone_number for h in
+        first.queue.hosts_with_phone_numbers
+    )
+    for p in phone_numbers:
+        logger.info('notify_queue_no_longer_empty: %s', p)
+        domain = Site.objects.get_current().domain
+        edit_path = reverse('edit', kwargs={'queue_id': first.queue.id})
+        edit_url = f"https://{domain}{edit_path}"
+        twilio.messages.create(
+            to=p,
+            from_=settings.TWILIO_PHONE_FROM,
+            body=(
+                f"Someone has joined your queue, {first.queue.name}! "
+                f"Please visit {edit_url} "
+                f"to start a meeting."
+            ),
         )
-        for p in phone_numbers:
-            logger.info('notify_queue_no_longer_empty: %s', p)
-            domain = Site.objects.get_current().domain
-            edit_path = reverse('edit', kwargs={'queue_id': first.queue.id})
-            edit_url = f"https://{domain}{edit_path}"
-            twilio.messages.create(
-                to=p,
-                from_=settings.TWILIO_PHONE_FROM,
-                body=(
-                    f"Someone has joined your queue, {first.queue.name}! "
-                    f"Please visit {edit_url} "
-                    f"to start a meeting."
-                ),
-            )
 
-    @receiver(pre_delete, sender=Meeting)
-    def trigger_notification_delete(sender, instance: Meeting, **kwargs):
-        if instance.line_place == 0:
-            if instance.queue.meeting_set.count() <= 1:
-                return
-            next_in_line = instance.queue.meeting_set.order_by('id')[1]
-            notify_next_in_line(next_in_line)
-
-    @receiver(post_save, sender=Meeting)
-    def trigger_notification_create(sender, instance: Meeting, created, **kwargs):
-        if instance.deleted or not created:
+@receiver(pre_delete, sender=Meeting)
+def trigger_notification_delete(sender, instance: Meeting, **kwargs):
+    if instance.line_place == 0:
+        if instance.queue.meeting_set.count() <= 1:
             return
-        if instance.line_place == 0:
-            notify_queue_no_longer_empty(instance)
+        next_in_line = instance.queue.meeting_set.order_by('id')[1]
+        notify_next_in_line(next_in_line)
+
+@receiver(post_save, sender=Meeting)
+def trigger_notification_create(sender, instance: Meeting, created, **kwargs):
+    if instance.deleted or not created:
+        return
+    if instance.line_place == 0:
+        notify_queue_no_longer_empty(instance)
--- src/officehours_api/tests.py
+++ src/officehours_api/tests.py
@@ -1,10 +1,11 @@
 from unittest import mock
 
-from django.test import TestCase, TransactionTestCase
+from django.test import TransactionTestCase, override_settings
 
 from officehours_api.models import User, Queue, Meeting
 
 
+@override_settings(TWILIO_ACCOUNT_SID='aaa', TWILIO_AUTH_TOKEN='bbb', TWILIO_PHONE_FROM='+15555552323')
 class NotificationTestCase(TransactionTestCase):
     def setUp(self):
         self.foo = User.objects.create(username='foo', email='foo@example.com')
@@ -37,67 +38,67 @@ class NotificationTestCase(TransactionTestCase):
         return m
 
     @staticmethod
-    def get_receivers(mock_twilio: mock.MagicMock):
-        return {c.kwargs['to'] for c in mock_twilio.mock_calls if 'to' in c.kwargs}
+    def get_receivers(MockTwilio: mock.MagicMock):
+        return {c.kwargs['to'] for c in MockTwilio.mock_calls if 'to' in c.kwargs}
 
-    @mock.patch('officehours_api.notifications.twilio')
-    def test_first_meeting_notifies_hosts(self, mock_twilio: mock.MagicMock):
+    @mock.patch('officehours_api.notifications.TwilioClient')
+    def test_first_meeting_notifies_hosts(self, MockTwilio: mock.MagicMock):
         self.create_meeting([self.foo, self.bar, self.baz])
-        self.assertEqual(mock_twilio.messages.create.call_count, 2)
-        receivers = self.get_receivers(mock_twilio)
+        self.assertEqual(MockTwilio().messages.create.call_count, 2)
+        receivers = self.get_receivers(MockTwilio)
         self.assertTrue(
             receivers >=
             {'+15555551111','+15555552222',}
         )
 
-    @mock.patch('officehours_api.notifications.twilio')
-    def test_first_meeting_doesnt_notify_attendees(self, mock_twilio: mock.MagicMock):
+    @mock.patch('officehours_api.notifications.TwilioClient')
+    def test_first_meeting_doesnt_notify_attendees(self, MockTwilio: mock.MagicMock):
         self.create_meeting([self.foo, self.bar, self.baz])
-        receivers = self.get_receivers(mock_twilio)
+        receivers = self.get_receivers(MockTwilio)
         self.assertFalse(
             receivers >=
             {'+15555550000', '+15555550001', '+15555550002',}
         )
 
-    @mock.patch('officehours_api.notifications.twilio')
-    def test_second_meeting_doesnt_notify_hosts(self, mock_twilio: mock.MagicMock):
+    @mock.patch('officehours_api.notifications.TwilioClient')
+    def test_second_meeting_doesnt_notify_hosts(self, MockTwilio: mock.MagicMock):
         self.create_meeting([self.foo,])
-        mock_twilio.reset_mock()
+        MockTwilio.reset_mock()
         self.create_meeting([self.bar, self.baz,])
-        receivers = self.get_receivers(mock_twilio)
+        receivers = self.get_receivers(MockTwilio)
         self.assertFalse(
             receivers >=
             {'+15555551111', '+15555552222',}
         )
 
-    @mock.patch('officehours_api.notifications.twilio')
-    def test_first_meeting_removal_notifies_next_in_line(self, mock_twilio: mock.MagicMock):
+    @mock.patch('officehours_api.notifications.TwilioClient')
+    def test_first_meeting_removal_notifies_next_in_line(self, MockTwilio: mock.MagicMock):
         m1 = self.create_meeting([self.foo,])
         self.create_meeting([self.bar, self.baz])
-        mock_twilio.reset_mock()
+        MockTwilio.reset_mock()
         m1.delete()
-        receivers = self.get_receivers(mock_twilio)
+        receivers = self.get_receivers(MockTwilio)
         self.assertTrue(
             receivers >=
             {'+15555550001', '+15555550002',}
         )
 
-    @mock.patch('officehours_api.notifications.twilio')
-    def test_second_meeting_removal_notifies_none(self, mock_twilio: mock.MagicMock):
+    @mock.patch('officehours_api.notifications.TwilioClient')
+    def test_second_meeting_removal_notifies_none(self, MockTwilio: mock.MagicMock):
         self.create_meeting([self.foo,])
         m2 = self.create_meeting([self.bar, self.baz])
-        mock_twilio.reset_mock()
+        MockTwilio.reset_mock()
         m2.delete()
-        receivers = self.get_receivers(mock_twilio)
+        receivers = self.get_receivers(MockTwilio)
         self.assertFalse('+15555550001' in receivers)
         self.assertFalse('+15555550002' in receivers)
 
-    @mock.patch('officehours_api.notifications.twilio')
-    def test_first_meeting_removal_doesnt_notify_second_in_line(self, mock_twilio: mock.MagicMock):
+    @mock.patch('officehours_api.notifications.TwilioClient')
+    def test_first_meeting_removal_doesnt_notify_second_in_line(self, MockTwilio: mock.MagicMock):
         m1 = self.create_meeting([self.foo,])
         self.create_meeting([self.bar,])
         self.create_meeting([self.baz,])
-        mock_twilio.reset_mock()
+        MockTwilio.reset_mock()
         m1.delete()
-        receivers = self.get_receivers(mock_twilio)
+        receivers = self.get_receivers(MockTwilio)
         self.assertFalse('+15555550002' in receivers)
