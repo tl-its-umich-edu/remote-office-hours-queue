e78c97e16b9864a15f2b8c2775ee15c2e20162d7
James Ostrander <jlost@umich.edu>
2020-10-15 08:12:37 -0400

    Remove unused imports

--- src/assets/src/components/edit.tsx
+++ src/assets/src/components/edit.tsx
@@ -4,8 +4,6 @@ import { Link } from "react-router-dom";
 import * as ReactGA from "react-ga";
 import { Alert, Button, Form, InputGroup, Modal, Table } from "react-bootstrap";
 import Dialog from "react-bootstrap-dialog";
-import PhoneInput from 'react-phone-input-2';
-import 'react-phone-input-2/lib/style.css';
 
 import * as api from "../services/api";
 import { User, QueueHost, Meeting, BluejeansMetadata, isQueueHost, QueueAttendee } from "../models";
