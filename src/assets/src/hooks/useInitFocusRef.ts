0aa9fb848ad46d28978d84cdb183855359db39eb
Sam Sciolla <ssciolla@umich.edu>
2020-10-30 14:55:27 -0400

    Switch order of ref assignment, early return

--- src/assets/src/hooks/useInitFocusRef.ts
+++ src/assets/src/hooks/useInitFocusRef.ts
@@ -1,8 +1,8 @@
 import { createRef, useEffect, RefObject } from "react";
 
 export function useInitFocusRef<T extends HTMLElement>(initFocus: boolean): RefObject<T> | undefined {
-    const ref = createRef<T>();
     if (!initFocus) return undefined;
+    const ref = createRef<T>();
     useEffect(() => {
         if (initFocus) ref.current!.focus();
     }, []);
