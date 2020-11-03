39cb58d58b5b86acb87843b28776895cd87bb64f
Sam Sciolla <ssciolla@umich.edu>
2020-11-03 16:17:39 -0500

    Add first draft of notification text adjustments

--- src/officehours_api/notifications.py
+++ src/officehours_api/notifications.py
@@ -1,4 +1,5 @@
 import logging
+from enum import Enum
 
 from django.conf import settings
 from django.dispatch import receiver
@@ -14,15 +15,31 @@ logger = logging.getLogger(__name__)
 
 twilio = TwilioClient(settings.TWILIO_ACCOUNT_SID, settings.TWILIO_AUTH_TOKEN)
 
+DOMAIN = Site.objects.get_current().domain
+
+
+class NotificationType(Enum):
+    HOST = 'host'
+    ATTENDEE = 'attendee'
+
+
+def create_notification_addendum(type: NotificationType) -> str:
+    pref_path = reverse('preferences')
+    pref_url = f"https://{DOMAIN}{pref_path}"
+    return (
+        f"\n\nThis message was sent by the University of Michigan ITS Remote Office Hours Queue "
+        f"because you opted in to receive {type.value} SMS notifications. "
+        f"To opt out or change your preferences, visit {pref_url}"
+    )
+
 
 def notify_next_in_line(next_in_line: Meeting):
     phone_numbers = list(
         u.profile.phone_number for u in
         next_in_line.attendees_with_phone_numbers.filter(profile__notify_me_attendee__exact=True)
     )
-    domain = Site.objects.get_current().domain
     queue_path = reverse('queue', kwargs={'queue_id': next_in_line.queue.id})
-    queue_url = f"https://{domain}{queue_path}"
+    queue_url = f"https://{DOMAIN}{queue_path}"
     for p in phone_numbers:
         try:
             logger.info('notify_next_in_line: %s', p)
@@ -32,19 +49,21 @@ def notify_next_in_line(next_in_line: Meeting):
                 body=(
                     f"You're next in line! Please visit {queue_url} "
                     f"for instructions to join."
+                    f"{create_notification_addendum(NotificationType.ATTENDEE)}"
                 ),
             )
         except:
             logger.exception(f"Error while sending attendee notification to {p} for queue {next_in_line.queue.id}")
+
 
 def notify_queue_no_longer_empty(first: Meeting):
     phone_numbers = list(
         h.profile.phone_number for h in
         first.queue.hosts_with_phone_numbers.filter(profile__notify_me_host__exact=True)
     )
-    domain = Site.objects.get_current().domain
     edit_path = reverse('edit', kwargs={'queue_id': first.queue.id})
-    edit_url = f"https://{domain}{edit_path}"
+    edit_url = f"https://{DOMAIN}{edit_path}"
+
     for p in phone_numbers:
         try:
             logger.info('notify_queue_no_longer_empty: %s', p)
@@ -53,11 +72,13 @@ def notify_queue_no_longer_empty(first: Meeting):
                 to=p,
                 body=(
                     f"Someone has joined your queue, {first.queue.name}! "
-                    f"Please visit {edit_url} to start a meeting."
+                    f"Please visit {edit_url} to see who joined and, if applicable, start the meeting."
+                    f"{create_notification_addendum(NotificationType.HOST)}"
                 ),
             )
         except:
             logger.exception(f"Error while sending host notification to {p} for queue {first.queue.id}")
+
 
 @receiver(pre_delete, sender=Meeting)
 def trigger_notification_delete(sender, instance: Meeting, **kwargs):
@@ -66,6 +87,7 @@ def trigger_notification_delete(sender, instance: Meeting, **kwargs):
             return
         next_in_line = instance.queue.meeting_set.order_by('id')[1]
         notify_next_in_line(next_in_line)
+
 
 @receiver(post_save, sender=Meeting)
 def trigger_notification_create(sender, instance: Meeting, created, **kwargs):
