0c4e43735c34aa6d7bb706d7851b0efe98366e63
Sam Sciolla <ssciolla@umich.edu>
2020-11-05 10:36:34 -0500

    Add Profile fields to User page on admin site

--- src/officehours_api/admin.py
+++ src/officehours_api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.models import User
 from safedelete.admin import SafeDeleteAdmin, highlight_deleted
-from officehours_api.models import Queue, Meeting, Attendee
+
+from officehours_api.models import Queue, Meeting, Attendee, Profile
 
 
 @admin.register(Queue)
@@ -21,3 +24,17 @@ class MeetingAdmin(admin.ModelAdmin):
     list_filter = ('queue',)
     search_fields = ['id', 'queue__name']
     inlines = (AttendeeInline,)
+
+
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+
+
+class UserAdmin(BaseUserAdmin):
+    inlines = (ProfileInline,)
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
