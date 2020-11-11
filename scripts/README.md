ac0ea69c50c314125b19fecee45ed34921f7d120
James Ostrander <jlost@umich.edu>
2020-11-11 10:18:57 -0500

    Update push scripts

---
+++ scripts/README.md
@@ -0,0 +1,7 @@
+# Scripts
+
+## Push Scripts
+
+The push scripts in this directory will build an image locally and push it to OpenShift's registry.
+They should only be used if remote builds in OpenShift are failing. 
+Login with `oc login ...` before running them.
--- scripts/push-prod.sh
+++ scripts/push-prod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
+# Login with `oc login ...` before running.
+oc project officehours
 docker login -u `oc whoami` -p `oc whoami -t` docker-registry.webplatformsunpublished.umich.edu
 docker build -t docker-registry.webplatformsunpublished.umich.edu/officehours/officehours .
 docker push docker-registry.webplatformsunpublished.umich.edu/officehours/officehours
-oc patch deployment officehours-web-prod -p \
-  "{\"spec\":{\"template\":{\"metadata\":{\"annotations\":{\"date\":\"`date +'%s'`\"}}}}}"
