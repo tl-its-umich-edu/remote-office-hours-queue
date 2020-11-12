e9b44ba52eac2615bfbb87351127ff452ab043dd
Sam Sciolla <ssciolla@umich.edu>
2020-11-12 16:48:06 -0500

    Introduce basic sorting of queues

---
+++ src/assets/src/sort.ts
@@ -0,0 +1,25 @@
+import { QueueBase } from "./models";
+
+type CompareQueueFunc = (a: QueueBase, b: QueueBase) => number;
+
+const compareQueueStatus: CompareQueueFunc = (a, b) => {
+    if (a.status === 'open' && b.status === 'closed') {
+        return -1;
+    } else if (a.status === 'closed' && b.status === 'open') {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+const compareQueueIDDesc: CompareQueueFunc = (a, b) => {
+    if (a.id > b.id) {
+        return -1;
+    } else {
+        return 1;
+    }
+}
+
+export const sortQueues = (queues: QueueBase[]): QueueBase[] => {
+    return queues.sort(compareQueueIDDesc).sort(compareQueueStatus)
+}
