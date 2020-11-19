ee5966a62c0c3311f7f8f4ec88e5539354a0a8fa
Sam Sciolla <ssciolla@umich.edu>
2020-11-19 13:34:39 -0500

    Use more specific var names with useEntityChanges in manage.tsx

--- src/assets/src/components/manage.tsx
+++ src/assets/src/components/manage.tsx
@@ -43,9 +43,9 @@ export function ManagePage(props: PageProps) {
     const oldQueues = usePreviousState(queues);
     const userWebSocketError = useUserWebSocket(props.user!.id, (u) => setQueues(u.hosted_queues));
 
-    const [changeEventMap, compareAndSetEventMap, popChangeEvent] = useEntityChanges<QueueBase>();
+    const [queueChangeEventMap, compareAndSetQueueChangeEventMap, popQueueChangeEvent] = useEntityChanges<QueueBase>();
     useEffect(() => {
-        if (queues && oldQueues) compareAndSetEventMap(oldQueues, queues);
+        if (queues && oldQueues) compareAndSetQueueChangeEventMap(oldQueues, queues);
     }, [queues])
 
     const errorSources = [
@@ -62,7 +62,7 @@ export function ManagePage(props: PageProps) {
             {errorDisplay}
             <h1>My Meeting Queues</h1>
             <p>These are all the queues you are a host of. Select a queue to manage it or add a queue below.</p>
-            <ChangeLog changeEventMap={changeEventMap} popChangeEvent={popChangeEvent} />
+            <ChangeLog changeEventMap={queueChangeEventMap} popChangeEvent={popQueueChangeEvent} />
             {queueTable}
             <hr/>
             <a target="_blank" href="https://documentation.its.umich.edu/node/1830">
