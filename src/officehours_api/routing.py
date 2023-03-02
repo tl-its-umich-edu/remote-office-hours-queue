d2b7ed279a34f1fef18c2802a3e6141782962aa4
Sam Sciolla <ssciolla@umich.edu>
2023-03-01 19:29:13 -0500

    Bump dependencies in Dockerfile; fix Channels; downgrade django-safedelete due to error

--- src/officehours_api/routing.py
+++ src/officehours_api/routing.py
@@ -3,6 +3,6 @@ from django.urls import re_path
 from . import consumers
 
 websocket_urlpatterns = [
-    re_path(r'ws/queues/(?P<queue_id>\w+)/$', consumers.QueueConsumer),
-    re_path(r'ws/users/(?P<user_id>\w+)/$', consumers.UserConsumer),
+    re_path(r'ws/queues/(?P<queue_id>\w+)/$', consumers.QueueConsumer.as_asgi()),
+    re_path(r'ws/users/(?P<user_id>\w+)/$', consumers.UserConsumer.as_asgi()),
 ]
