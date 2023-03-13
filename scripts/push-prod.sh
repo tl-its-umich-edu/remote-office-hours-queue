b052a6a58ebc900e4b9d662bb44252ab66304287
Sam Sciolla <ssciolla@umich.edu>
2023-03-13 13:44:06 -0400

    Add platform flag with amd

--- scripts/push-prod.sh
+++ scripts/push-prod.sh
@@ -4,5 +4,5 @@ set -e
 # Login with `oc login ...` before running.
 oc project officehours
 docker login -u `oc whoami` -p `oc whoami -t` docker-registry.webplatformsunpublished.umich.edu
-docker build -t docker-registry.webplatformsunpublished.umich.edu/officehours/officehours .
+docker build --platform linux/amd64 -t docker-registry.webplatformsunpublished.umich.edu/officehours/officehours .
 docker push docker-registry.webplatformsunpublished.umich.edu/officehours/officehours
