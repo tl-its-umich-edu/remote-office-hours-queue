639ccc35684a70c873517ffc1f3dae2310cfe133
Sam Sciolla <ssciolla@umich.edu>
2023-03-16 16:31:07 -0400

    Update Node, Webpack, TypeScript; fix errors from TS, Webpack; remove unneeded packages; run npm audit fix

--- src/assets/src/components/page.ts
+++ src/assets/src/components/page.ts
@@ -1,7 +1,7 @@
 import { RouteComponentProps } from "react-router";
 import { MeetingBackend, User } from "../models";
 
-export interface PageProps<TParams = {}> extends RouteComponentProps<TParams> {
+export interface PageProps<TParams extends { [K in keyof TParams]?: string | undefined; } = {}> extends RouteComponentProps<TParams> {
     user?: User;
     loginUrl: string;
     backends: MeetingBackend[];
