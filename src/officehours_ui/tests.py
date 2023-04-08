df728db9ce4d2260af6d9bb684133d5e13f84d32
lialex111 <lialex111@yahoo.com>
2023-04-08 14:02:33 -0400

    fixed issues 369 and 298

---
+++ src/officehours_ui/tests.py
@@ -0,0 +1,12 @@
+from django.test import TestCase, Client
+
+class UITestCase(TestCase):
+    def test_robots_txt(self):
+        c = Client()
+        response = c.get('/robots.txt')
+        self.assertEqual(response.status_code, 200)
+
+    def test_favicon_ico(self):
+        c = Client()
+        response = c.get('/static/favicon.ico')
+        self.assertEqual(response.status_code, 200)
