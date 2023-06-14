295b7141a3792116dc920871902ad86c3520a7f0
Sam Sciolla <ssciolla@umich.edu>
2023-06-13 20:48:22 -0400

    Tidy up views TestCase, renaming users, removing unused imports, etc.

--- src/officehours_api/test_views.py
+++ src/officehours_api/test_views.py
@@ -1,62 +1,51 @@
 # started code reference from remote-office-hours-queue/src/officehours_api/tests.py
 import json
 
-from django.contrib.auth.models import AnonymousUser, User
+from django.contrib.auth.models import User
 from django.test import Client, TestCase
-from rest_framework.test import APIRequestFactory
+from rest_framework import status
 
-from officehours_api.models import User, Queue, Meeting
-from officehours_api.views import MeetingDetail
+from officehours_api.models import Meeting, Queue
 
 
 class MeetingTestCase(TestCase):
 
     def setUp(self):
-        self.foo = User.objects.create(username='foo', email='foo@example.com')
-        self.bar = User.objects.create(username='bar', email='bar@example.com')
-        self.baz = User.objects.create(username='baz', email='baz@example.com')
-        self.baz.set_password('rohqtest')
-        self.baz.save()
-        self.hostie = User.objects.create(
-            username='hostie', email='hostie@example.com')
-        self.queue = Queue.objects.create(
-            name='MeetingTest',
+        self.attendee_one = User.objects.create(
+            username='attendeeone', email='attendeeone@example.com'
         )
-        self.queue.hosts.set([self.hostie, self.baz])
+        self.host_one = User.objects.create(
+            username='hostone', email='hostone@example.com'
+        )
+        self.host_one.set_password('rohqtest')
+        self.host_one.save()
+        self.host_two = User.objects.create(
+            username='hosttwo', email='hosttwo@example.com'
+        )
+        self.queue = Queue.objects.create(name='Test Queue')
+        self.queue.hosts.set([self.host_one, self.host_two])
         self.queue.save()
-        self.exceptions = 0
         self.client = Client()
 
-    # changed the create meeting from the one already in test.py (changes a bit)
-    def create_meeting(self, host, attendees, start=False):
-        m = Meeting.objects.create(
-            queue=self.queue,
-            backend_type='inperson',
-        )
-        m.attendees.set(attendees)
-        if start:
-            m.assignee = host
-            m.start()
-            m.save()
-        return m
+    def test_cannot_reassign_host_when_meeting_has_started(self):
+        meeting = Meeting.objects.create(queue=self.queue, backend_type='inperson')
+        meeting.attendees.set([self.attendee_one])
+        meeting.assignee = self.host_two
+        meeting.start()
+        meeting.save()
 
-    def test_error(self):
-        # meeting has been created where the host is self.hostie and attendees are self.foo and self.bar , the nature of the meeting is zoom
-        meeting_in_progress = self.create_meeting(self.hostie, [self.bar], True)
-        # put request to change the assignee of the meeting (not understandig what to send in this and how)
-        url = f'/api/meetings/{meeting_in_progress.id}/'
+        url = f'/api/meetings/{meeting.id}/'
         data = {
-            "attendee_ids": [self.bar.id],
-            "assignee_id": self.baz.id
+            "attendee_ids": [self.attendee_one.id],
+            "assignee_id": self.host_one.id
         }
-        
-        login_result = self.client.login(username='baz', password='rohqtest')
+        self.client.login(username='hostone', password='rohqtest')
         response = self.client.put(url, data, content_type='application/json')
 
-        # view response
-        print(response)
-        print(response.content)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
         response_body = json.loads(response.content)
-
-        self.assertEqual(response_body['Meeting Detail'], "Can't change assignee once meeting is started!")
+        self.assertEqual(
+            response_body['Meeting Detail'],
+            "Can't change assignee once meeting is started!"
+        )
