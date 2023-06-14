516c9fbfcc09de1c117e4f0d54e25eb605603b51
Sam Sciolla <ssciolla@umich.edu>
2023-06-13 21:14:53 -0400

    Add additional test case for changing backend_type

--- src/officehours_api/test_views.py
+++ src/officehours_api/test_views.py
@@ -1,10 +1,12 @@
 # started code reference from remote-office-hours-queue/src/officehours_api/tests.py
 import json
+from unittest import skipIf
 
 from django.contrib.auth.models import User
 from django.test import Client, TestCase
 from rest_framework import status
 
+from officehours.settings import ENABLED_BACKENDS
 from officehours_api.models import Meeting, Queue
 
 
@@ -22,7 +24,9 @@ class MeetingTestCase(TestCase):
         self.host_two = User.objects.create(
             username='hosttwo', email='hosttwo@example.com'
         )
-        self.queue = Queue.objects.create(name='Test Queue')
+        self.queue = Queue.objects.create(
+            name='Test Queue', allowed_backends=['inperson', 'zoom']
+        )
         self.queue.hosts.set([self.host_one, self.host_two])
         self.queue.save()
 
@@ -49,4 +53,23 @@ class MeetingTestCase(TestCase):
         self.assertEqual(
             response_body['Meeting Detail'],
             "Can't change assignee once meeting is started!"
+        )
+
+    @skipIf('zoom' not in ENABLED_BACKENDS, 'Skipping because "zoom" backend type is not enabled')
+    def test_cannot_change_backend_type_when_meeting_has_started(self):
+        url = f'/api/meetings/{self.meeting.id}/'
+        data = {
+            "attendee_ids": [self.attendee_one.id],
+            "assignee_id": self.host_one.id,
+            "backend_type": "zoom"
+        }
+        self.client.login(username='hostone', password='rohqtest')
+        response = self.client.put(url, data, content_type='application/json')
+
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+        response_body = json.loads(response.content)
+        self.assertEqual(
+            response_body['Meeting Detail'],
+            "Can't change backend_type once meeting is started!"
         )
