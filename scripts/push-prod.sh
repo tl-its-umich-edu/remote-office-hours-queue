7b0462ada7ad75bef5660135d4029fb362a2f03e
Code Hugger (Matthew Jones) <jonespm@umich.edu>
2024-02-15 14:41:40 -0500

    #456 - Renames qa to test and updates some other variables as part of ROSA migration (#457)
    * Fixes #456 - Renames qa to test anad updates some other variables as
    part of ROSA
    
    * Some more updates switching over qa to test.
    
    QA domain name is still there as test hasn't been created yet
    
    * Have to leave these hostnames as qa for now until we get the test
    hostname and switch over.
    
    * Updated version for autoscaler
    Updated deployment location

--- scripts/push-prod.sh
+++ scripts/push-prod.sh
@@ -3,6 +3,6 @@ set -e
 
 # Login with `oc login ...` before running.
 oc project officehours
-docker login -u `oc whoami` -p `oc whoami -t` docker-registry.webplatformsunpublished.umich.edu
-docker build --platform linux/amd64 -t docker-registry.webplatformsunpublished.umich.edu/officehours/officehours .
-docker push docker-registry.webplatformsunpublished.umich.edu/officehours/officehours
+docker login -u `oc whoami` -p `oc whoami -t` registry.aws.web.umich.edu
+docker build --platform linux/amd64 -t registry.aws.web.umich.edu/officehours/officehours .
+docker push registry.aws.web.umich.edu/officehours/officehours
