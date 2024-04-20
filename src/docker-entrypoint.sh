b9e9c0490742d38eb800d97ba160691988282d38
Peijing Li <peijli@umich.edu>
2024-04-20 14:31:03 -0400

    Remove temporary testing token and temporary bash scripts

--- src/docker-entrypoint.sh
+++ src/docker-entrypoint.sh
@@ -2,10 +2,6 @@
 
 set -e
 
-# I don't know why, but the following line is necessary to avoid a "ModuleNotFoundError: No module named 'pyzoom'" error
-# Simply adding 'pyzoom' to the requirements.txt file doesn't work
-python -m pip install --no-cache-dir --upgrade pyzoom
-
 python manage.py collectstatic --noinput
 python manage.py migrate
 
