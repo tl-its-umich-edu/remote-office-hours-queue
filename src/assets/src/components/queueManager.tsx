b44beea0cc9bc1b1a18f7387bd9490395e02a0bc
jxiao21 <newsap98@gmail.com>
2024-06-07 11:36:57 -0400

    updated same changes for manage

--- src/assets/src/components/queueManager.tsx
+++ src/assets/src/components/queueManager.tsx
@@ -250,7 +250,7 @@ export function QueueManagerPage(props: PageProps) {
     let { queue_id } = useParams()
     if (queue_id === undefined) throw new Error("queue_id is undefined!");
     if (!props.user) throw new Error("user is undefined!");
-    const queueIdParsed = parseInt(queue_id);
+    // const queueIdParsed = parseInt(queue_id);
 
     // Set up basic state
     const [queue, setQueue] = useState(undefined as QueueHost | undefined);
@@ -266,7 +266,8 @@ export function QueueManagerPage(props: PageProps) {
             setAuthError(new Error("You are not a host of this queue. If you believe you are seeing this message in error, contact the queue host(s)."));
         }
     }
-    const queueWebSocketError = useQueueWebSocket(queueIdParsed, setQueueChecked);
+    // const queueWebSocketError = useQueueWebSocket(queueIdParsed, setQueueChecked);
+    const queueWebSocketError = useQueueWebSocket(queue_id, setQueueChecked);
     const [visibleMeetingDialog, setVisibleMeetingDialog] = useState(undefined as Meeting | undefined);
 
     const [myUser, setMyUser] = useState(undefined as MyUser | undefined);
@@ -353,7 +354,8 @@ export function QueueManagerPage(props: PageProps) {
             <Dialog {...dialogState} />
             <LoginDialog visible={loginDialogVisible} loginUrl={props.loginUrl} />
             <MeetingInfoDialog backends={props.backends} meeting={visibleMeetingDialog} onClose={() => setVisibleMeetingDialog(undefined)} />
-            <Breadcrumbs currentPageTitle={queue?.name ?? queueIdParsed.toString()} intermediatePages={[{title: "Manage", href: "/manage"}]} />
+            {/* <Breadcrumbs currentPageTitle={queue?.name ?? queueIdParsed.toString()} intermediatePages={[{title: "Manage", href: "/manage"}]} /> */}
+            <Breadcrumbs currentPageTitle={queue?.name ?? queue_id.toString()} intermediatePages={[{title: "Manage", href: "/manage"}]} />
             {loadingDisplay}
             {errorDisplay}
             {queueManager}
