8bc992ba87e0e85e0946efb7fa332a9c1666ed00
jad <alaoui@umich.edu>
2024-12-06 11:13:35 -0500

    Fixes #559: Added an extra assert statement to ensure EMAIL_SUBJECT_PREFIX doesn't contain a newline character

--- src/officehours/test_settings.py
+++ src/officehours/test_settings.py
@@ -78,6 +78,7 @@ class ChannelLayersConfigTest(TestCase):
             self.assertEqual(settings.EMAIL_HOST, settings.EMAIL_HOST.strip())
         self.assertTrue(settings.EMAIL_SUBJECT_PREFIX.startswith('['))
         self.assertTrue(settings.EMAIL_SUBJECT_PREFIX.endswith('] '))
+        self.assertEqual(settings.EMAIL_SUBJECT_PREFIX, settings.EMAIL_SUBJECT_PREFIX.strip())
 
     def test_admins_and_managers(self):
         self.assertIsInstance(settings.ADMINS, list)
