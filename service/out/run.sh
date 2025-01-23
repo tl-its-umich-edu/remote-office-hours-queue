bbf268e20cafae7c07e093303312b568c23e97ef
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2025-01-23 16:47:43 -0500

    #561 - improve and document `run.sh` script

--- service/out/run.sh
+++ service/out/run.sh
@@ -1,9 +1,16 @@
 #!/bin/sh --
 
-# gcsplit installed via `brew install coreutils`
-(cd ..; kustomize build overlays/prod) | gcsplit -s - /^---$/ '{*}'
-# optional: use csplit instead of gcsplit, but update the count (7) as needed
-# (cd ..; kustomize build overlays/prod) | csplit -s - '/^---$/' '{7}'
+# gcsplit may be installed via `brew install coreutils`
+splitter='gcsplit'
+repeat='{*}'
+# use `csplit` if default splitter is not available
+if ! command -v ${splitter} > /dev/null; then
+  splitter='csplit'
+  repeat='{7}'
+fi
+
+(cd ..; kustomize build overlays/prod) | $splitter -s - /^---$/ $repeat
+
 for i in xx*; do
   sed '/^---$/,1d' $i > $(yq '.kind + "-" + .metadata.name + ".yaml"' $i)
 done
