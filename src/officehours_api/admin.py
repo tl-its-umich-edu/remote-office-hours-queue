467d50dfdb0a83843714f91be0d33208ca11109e
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2025-02-07 14:14:19 -0500

    #521 - cleanup and clarifications

--- src/officehours_api/admin.py
+++ src/officehours_api/admin.py
@@ -1,6 +1,4 @@
-import csv
 import logging
-from pickletools import long1
 
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
@@ -41,14 +39,13 @@ class ExporterAdminMixin:
         :return:  CSV file of the queues represented by the selection.
         """
 
-        meta = self.model._meta
         queues_queryset = self.queues_queryset(request, queryset)
         queue_ids = list(map(lambda λ: λ.id, queues_queryset))
-        # logger.info(f'Exporting {meta} for queues {repr(list(queue_ids))}')
 
         if len(queue_ids) > 0:
             response = HttpResponse(content_type='text/csv')
-            response['Content-Disposition'] = f'attachment; filename={meta}.csv'
+            response['Content-Disposition'] = \
+                'attachment; filename=meeting_data.csv'
 
             ExportMeetingStartLogs.extract_log(queue_ids, response)
 
@@ -58,11 +55,21 @@ class ExporterAdminMixin:
 
 @admin.register(Queue)
 class QueueAdmin(ExporterAdminMixin, SafeDeleteAdmin):
-    list_display = ('id', highlight_deleted, 'created_at', 'status') + SafeDeleteAdmin.list_display
+    list_display = (('id', highlight_deleted, 'created_at', 'status') +
+                    SafeDeleteAdmin.list_display)
     list_filter = ('hosts', 'status',) + SafeDeleteAdmin.list_filter
     search_fields = ['id', 'name']
 
     def queues_queryset(self, request, selection_queryset):
+        """
+        In the Queue model, this queryset will already contain only Queues,
+        so it can be returned directly.
+
+        :param request: Django request object.
+        :param selection_queryset: A queryset of objects for which queues will
+          be found.
+        :return: A queryset of queues represented by the selection.
+        """
         return selection_queryset
 
 
@@ -89,13 +96,24 @@ class UserAdmin(ExporterAdminMixin, BaseUserAdmin):
     inlines = (ProfileInline,)
 
     def queues_queryset(self, request, selection_queryset):
+        """
+        Take a User selection queryset and return a Queue queryset related to
+        it if the User is a host of any Meeting in Queues.  If the User is
+        not a host of any Meeting in Queues, display an error message and
+        return an empty queryset.
+
+        :param request: Django request object.
+        :param selection_queryset: A User queryset for which queues
+          will be found.
+        :return: A Queue queryset related to the selection.
+        """
         queues = Queue.objects.filter(hosts__in=selection_queryset)
         if len(queues) == 0:
             uniqnames = list(map(lambda λ: λ.username, selection_queryset))
             self.message_user(
                 request=request,
                 message='No queues were found with the selected '
-                        f'user{'s' if len(uniqnames) > 1 else ''} as host: '
+                        f'user{'s' * (len(uniqnames) > 1)} as host: '
                         f'({', '.join(uniqnames)})',
                 level='ERROR'
             )
