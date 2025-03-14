c4173e327ecbc5d7653c76339dedd0dbf1700640
Jaydon Krooss <jkrooss@umich.edu>
2025-03-14 14:12:25 -0400

    #635 cookie consent rework - use react helmet async

--- src/assets/src/containers/app.tsx
+++ src/assets/src/containers/app.tsx
@@ -11,7 +11,7 @@ import { QueuePage } from "../components/queue";
 import { QueueManagerPage } from "../components/queueManager";
 import { ManageQueueSettingsPage } from "../components/queueSettings";
 import { SearchPage } from "../components/search";
-import { InitializeConsentManagerParams, useGoogleAnalytics, useUmConsent } from "@tl-its-umich-edu/react-ga-onetrust-consent";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 
 
 interface AppProps {
@@ -19,21 +19,6 @@ interface AppProps {
 }
 
 export function App(props: AppProps) {
-    const { gaInitialized, gaHandlers } = useGoogleAnalytics({
-        googleAnalyticsId: props.globals.ga_tracking_id, 
-        debug: props.globals.debug,
-    });
-
-    const { umConsentInitialize, umConsentInitialized } = useUmConsent();    
-    if (!umConsentInitialized && gaInitialized && gaHandlers.onConsentApprove && gaHandlers.onConsentReject) {
-        const consentParams: InitializeConsentManagerParams = {
-            developmentMode: false,
-            alwaysShow: false,
-            onConsentApprove: gaHandlers.onConsentApprove,
-            onConsentReject: gaHandlers.onConsentReject,
-        }
-        umConsentInitialize(consentParams);
-    }
 
     const commonProps: PageProps = {
         user: props.globals.user,
@@ -44,15 +29,34 @@ export function App(props: AppProps) {
     };
 
     return (
-        <Routes>
-            <Route path="/" element={<HomePage {...commonProps} />} />
-            <Route path="/manage" element={<ManagePage {...commonProps} />} />
-            <Route path="/manage/:queue_id" element={<QueueManagerPage {...commonProps} />} />
-            <Route path="/manage/:queue_id/settings" element={<ManageQueueSettingsPage {...commonProps} />} />
-            <Route path="/queue/:queue_id" element={<QueuePage {...commonProps} />} />
-            <Route path="/search/" element={<SearchPage {...commonProps} />} />
-            <Route path="/preferences" element={<PreferencesPage {...commonProps} />} />
-            <Route path="/add_queue" element={<AddQueuePage {...commonProps} />} />
-        </Routes>
+        <>
+            <Routes>
+                <Route path="/" element={<HomePage {...commonProps} />} />
+                <Route path="/manage" element={<ManagePage {...commonProps} />} />
+                <Route path="/manage/:queue_id" element={<QueueManagerPage {...commonProps} />} />
+                <Route path="/manage/:queue_id/settings" element={<ManageQueueSettingsPage {...commonProps} />} />
+                <Route path="/queue/:queue_id" element={<QueuePage {...commonProps} />} />
+                <Route path="/search/" element={<SearchPage {...commonProps} />} />
+                <Route path="/preferences" element={<PreferencesPage {...commonProps} />} />
+                <Route path="/add_queue" element={<AddQueuePage {...commonProps} />} />
+            </Routes>
+            <HelmetProvider>
+                <Helmet>
+                    <script type="text/javascript">
+                    {`window.umConsentManager = {
+                        googleAnalyticsID: ${props.globals.ga_tracking_id ?`'${props.globals.ga_tracking_id}'`: false},
+                        googleAnalyticsCustom: {
+                            streamConfig: {
+                                cookie_flags: "SameSite=None; Secure"
+                            }
+                        },
+                        externalLinkBlank: true
+                        }
+                    `}  
+                    </script>
+                    <script async src="https://umich.edu/apis/umconsentmanager/consentmanager.js"/>
+                </Helmet>
+            </HelmetProvider>
+        </>
     );
 }
