f62f00fe5dc0c877debf808096bd2cc28977a8d5
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2025-06-09 13:52:54 -0400

    #627 - reduce repeated code; sort displayed values

--- src/officehours_api/admin_filters.py
+++ src/officehours_api/admin_filters.py
@@ -1,39 +1,46 @@
 from django.contrib.admin import SimpleListFilter
 from django.contrib.auth.models import User
+
 from officehours_api.models import Queue
 
 
-class ActiveHosts(SimpleListFilter):
+class BaseActiveFilter(SimpleListFilter):
+    def lookups(self, request, _):
+        related_objects = self.get_objects()
+        return sorted(
+            [(obj.id, obj.name if hasattr(obj, 'name') else obj.username) for
+             obj in related_objects], key=lambda λ: λ[1])
+
+    def queryset(self, request, queryset):
+        if self.value():
+            try:
+                obj_id = int(self.value())
+                return queryset.filter(**{self.filter_key: obj_id})
+            except ValueError:
+                return queryset.none()
+        return queryset
+
+    def get_objects(self):
+        raise NotImplementedError('Subclasses must implement `get_objects`')
+
+    @property
+    def filter_key(self):
+        raise NotImplementedError('Subclasses must define `filter_key`')
+
+
+class ActiveHosts(BaseActiveFilter):
     title = 'hosts'
     parameter_name = 'hosts'
+    filter_key = 'hosts__id'
 
-    def lookups(self, request, _):
-        host_users = User.objects.filter(queue__isnull=False).distinct()
-        return [(u.id, u.username) for u in host_users]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            try:
-                host_id = int(self.value())
-                return queryset.filter(hosts__id=host_id)
-            except ValueError:
-                return queryset.none()
-        return queryset
+    def get_objects(self):
+        return User.objects.filter(queue__isnull=False).distinct()
 
 
-class ActiveQueues(SimpleListFilter):
+class ActiveQueues(BaseActiveFilter):
     title = 'queues'
     parameter_name = 'queue'
+    filter_key = 'queue__id'
 
-    def lookups(self, request, _):
-        meeting_queues = Queue.objects.filter(meeting__isnull=False).distinct()
-        return [(q.id, q.name) for q in meeting_queues]
-
-    def queryset(self, request, queryset):
-        if self.value():
-            try:
-                queue_id = int(self.value())
-                return queryset.filter(queue__id=queue_id)
-            except ValueError:
-                return queryset.none()
-        return queryset
+    def get_objects(self):
+        return Queue.objects.filter(meeting__isnull=False).distinct()
