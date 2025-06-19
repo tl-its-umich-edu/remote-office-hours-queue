685a53ef71ff1c477d1feedb6e158afc1ea675f4
Mr. Lance E Sloan (lsloan) <lsloan-github.com@umich.edu>
2025-06-19 11:16:34 -0400

    #520 - changes requested by @zqian
    There's a possibility that the reverse SQL will fail because the migration SQL drops the view so that it can recreate the view with columns in a different order.

--- src/officehours_api/migrations/0030_update_simpler_meeting_start_logs_view.py
+++ src/officehours_api/migrations/0030_update_simpler_meeting_start_logs_view.py
@@ -42,22 +42,20 @@ class Migration(migrations.Migration):
                     parsed_response
                 JOIN officehours_api_queue q ON (response -> 'queue')::int = q.id
             """,
-            # NOTE: This SQL reverts the view to its state BEFORE migration 0029.
             reverse_sql="""
-                DROP VIEW meeting_start_logs;
-
-                CREATE VIEW meeting_start_logs AS
+                CREATE OR REPLACE VIEW meeting_start_logs AS
                 WITH parsed_response AS (
                     SELECT
-                        response::jsonb AS response
+                        response::jsonb AS response,
+                        (response::jsonb -> 'queue')::int AS queue_id
                     FROM
                         rest_framework_tracking_apirequestlog
                     WHERE
                         view::text = 'officehours_api.views.MeetingStart'
                         AND (response::jsonb ->> 'created_at') IS NOT NULL
                 )
-                SELECT
-                    (response -> 'queue')::int AS queue_id,
+                SELECT DISTINCT
+                    pr.queue_id AS queue_id,
                     (response -> 'attendees' -> 0 ->> 'id')::int AS attendee_id,
                     (response -> 'attendees' -> 0 ->> 'user_id')::int AS attendee_user_id,
                     response -> 'attendees' -> 0 ->> 'username' AS attendee_uniqname,
@@ -70,9 +68,12 @@ class Migration(migrations.Migration):
                     response -> 'backend_type' AS meeting_type,
                     response -> 'backend_metadata' ->> 'meeting_url' AS meeting_url,
                     response -> 'agenda' AS agenda,
-                    to_timestamp(response::jsonb ->> 'created_at'::text, 'YYYY-MM-DD"T"HH24:MI:SS.US'::text) AS meeting_created_at
+                    to_timestamp(response::jsonb ->> 'created_at'::text, 'YYYY-MM-DD"T"HH24:MI:SS.US'::text) AS meeting_created_at,
+                    q.name AS queue_name,
+                    q.status AS queue_status
                 FROM
-                    parsed_response
+                    parsed_response pr
+                JOIN officehours_api_queue q ON pr.queue_id = q.id
             """
         ),
     ]
